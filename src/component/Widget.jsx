937e78bb81adbe6758a755d5476e7f82ae5c0aba
Victor <user@Victors-MacBook-Pro.local>
2024-06-24 11:59:20 +0100

    widget fix

--- src/component/Widget.jsx
+++ src/component/Widget.jsx
@@ -1,12 +1,55 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import "../../src/index.css";
+
+import { Suspense } from "react";
+import { getRPC } from "../lib//getRPC";
 import { SwapWidget } from '@uniswap/widgets'
-import '@uniswap/widgets/fonts.css'
+
+
+const TOKEN_LIST = "https://ipfs.io/ipns/tokens.uniswap.org";
+
+const UNI = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
 
 const Widget = () => {
-  return (
-    <div className="Uniswap">
-    <SwapWidget />
-  </div>
-  )
-}
+  if (typeof window !== "undefined") {
+    window.Browser = {
+      T: () => {},
+    };
+  }
 
-export default Widget
+
+  const settings = {
+    jsonRpcUrlMap: {
+      1: [getRPC(1)],
+    },
+    tokenList: TOKEN_LIST,
+    defaultInputTokenAddress: "NATIVE",
+    defaultInputAmount: "1",
+    defaultOutputTokenAddress: UNI,
+  };
+
+  const theme =  {
+    primary: '#FFF',
+    secondary: '#A9A9A9',
+    interactive: '#000000',
+    container: '#000000',
+    module: '#222633',
+    accent: '#3B9239',
+    outline: '#CC1',
+    dialog: '#000',
+    fontFamily: 'Josefin Sans',
+    borderRadius: 0.5,
+  }
+
+  return (
+    <main className="w-full flex justify-center">
+      <Suspense 
+        fallback={<AiOutlineLoading3Quarters className="animate-spin" />}
+      >
+        <SwapWidget {...settings} theme={theme} width='100%'/>
+      </Suspense>
+    </main>
+  );
+};
+
+export default Widget;
